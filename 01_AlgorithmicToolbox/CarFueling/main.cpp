5a09f5b2e479611f7f98cb09ce8ffade6778bb93
rbehrou <reza.behrou@colorado.edu>
2022-06-05 09:13:45 -0700

    added a routine for car fueling problem.

--- 01_AlgorithmicToolbox/CarFueling/main.cpp
+++ 01_AlgorithmicToolbox/CarFueling/main.cpp
@@ -2,27 +2,50 @@
 #include <stdlib.h> /* srand, rand */
 #include <limits.h>
 #include <vector>
+#include <algorithm>
+
+// helper function to generate vector of sorted random nubmer
+std::vector<int> genRand(const int &MaxSize, const int &MaxRand)
+{
+    // generate random vector of integer and sort it
+    std::vector<int> vec(MaxSize);
+    vec[0] = 0;
+    for (auto i = 1; i < MaxSize; ++i)
+    {
+        vec[i] = rand() % MaxRand;
+    }
+    sort(vec.begin(), vec.end());
+    return vec;
+};
 
 // get number of required station (greedy algorithm)
-int getNumRefillsGRD(const std::vector<double> &x, const double &L)
+int getNumRefillsGRD(const std::vector<int> &x, const int &L)
 {
     // initialize
     auto n = x.size();
     auto CurrRefill = 0;
     auto NumRefills = 0;
-    while (CurrRefill <= n)
+    while (CurrRefill < n)
     {
         auto LastRefill = CurrRefill;
-        // traverse along the stations
-        while (CurrRefill <= n && (x[CurrRefill + 1] - x[LastRefill]) <= L)
+        // traverse along the stations and update
+        while (CurrRefill < n && (x[CurrRefill + 1] - x[LastRefill]) <= L)
             CurrRefill += 1;
         // check for impossible case
         if (CurrRefill == LastRefill)
         {
+            std::cout << "-------------------------------------------------------------" << std::endl;
+            std::cout << "CurrRefill: " << CurrRefill << ", LastRefill: " << LastRefill << ", NumRefills: " << NumRefills << std::endl;
+            std::cout << "L is: " << L << std::endl;
+            for (auto j = 0; j < x.size(); ++j)
+            {
+                std::cout << x[j] << std::endl;
+            }
             std::cout << "the setup for current configuration is impossible!" << std::endl;
+            std::cout << "-------------------------------------------------------------" << std::endl;
             return 0;
         }
-        if (CurrRefill <= n)
+        if (CurrRefill < n)
             NumRefills += 1;
     }
     // return total number of refills
@@ -32,15 +55,33 @@ int getNumRefillsGRD(const std::vector<double> &x, const double &L)
 int main()
 {
     // test cases
-    std::vector<std::vector<double>> test_vec{{0.0, 200.0, 375.0, 550.0, 750.0, 950.0}};
-    std::vector<double> l_vec {400.0};
+    std::vector<std::vector<int>> test_vec{{genRand(10, 1000)},
+                                           {genRand(10, 1000)},
+                                           {genRand(10, 1000)},
+                                           {genRand(10, 1000)},
+                                           {genRand(10, 1000)},
+                                           {genRand(10, 1000)},
+                                           {genRand(10, 1000)},
+                                           {genRand(10, 1000)},
+                                           {genRand(10, 1000)},
+                                           {genRand(10, 1000)}};
+    std::vector<int> l_vec{250 +  (rand() % 100),
+                           250 +  (rand() % 100),
+                           250 +  (rand() % 100),
+                           250 +  (rand() % 100),
+                           250 +  (rand() % 100),
+                           250 +  (rand() % 100),
+                           250 +  (rand() % 100),
+                           250 +  (rand() % 100),
+                           250 +  (rand() % 100),
+                           250 +  (rand() % 100)};
 
     // loop over number of test cases and call the gcd
     std::cout << "-------------------------------------------------------------" << std::endl;
     for (auto i = 0; i < test_vec.size(); ++i)
     {
         auto NumFills = getNumRefillsGRD(test_vec[i], l_vec[i]);
-        std::cout << "The total number of fills is: " << NumFills << std::endl;
+        std::cout << "The total number of refill is: " << NumFills << std::endl;
     }
     std::cout << "-------------------------------------------------------------" << std::endl;
     return 0;
