a62b0aed3005ec14d3f59be4d5f66fa6fff86c44
rbehrou <reza.behrou@colorado.edu>
2022-06-06 20:02:38 -0700

    added the implementation for merge sort.

---
+++ 01_AlgorithmicToolbox/MergeSort/README.md
@@ -0,0 +1,4 @@
+# Selection sort:
+
+The merge sort technique is based on divide and conquer technique. We divide the while data set into smaller parts and merge them into a larger piece in sorted order. It is also very effective for worst cases because this algorithm has lower time complexity for worst case also.
+
--- 01_AlgorithmicToolbox/MergeSort/main.cpp
+++ 01_AlgorithmicToolbox/MergeSort/main.cpp
@@ -32,29 +32,94 @@ void printVec(const std::vector<int> &x)
     std::cout << "]" << std::endl;
 };
 
-// selection sort
-void SelectionSort(std::vector<int> &x)
+std::vector<int> slice(std::vector<int> const &v, int m, int n)
 {
-    // loop
-    for (auto i = 0; i < x.size(); ++i)
+    auto first = v.cbegin() + m;
+    auto last = v.cbegin() + n;
+
+    std::vector<int> vec(first, last);
+    return vec;
+};
+
+std::vector<int> Merge(std::vector<int> &v1, std::vector<int> &v2)
+{
+
+    // v3 is the output vector
+    // it will store the merged vector obtained by merging v1 and v2
+    std::vector<int> v3;
+
+    // initialize
+    auto i = 0;
+    auto j = 0;
+    auto n = v1.size();
+    auto m = v2.size();
+
+    // traverse each elemenst of v1 and v2
+    while (i < n && j < m)
     {
-        auto mid = i;
-        for (auto j = i + 1; j < x.size(); ++j)
+
+        // comparing v1[i] and v2[j]
+        // if v1[i] is smaller than v2[j]
+        // push v1[i] to v3 and increment i
+        // if v2[j] is smaller than v1[i]
+        // push v2[j] to v3 and increment j
+        if (v1[i] <= v2[j])
         {
-            if (x[j] < x[mid])
-            {
-                mid = j;
-            }
+            v3.push_back(v1[i]);
+            ++i;
         }
-        // swap two indices
-        std::swap(x[i], x[mid]);
+        else
+        {
+            v3.push_back(v2[j]);
+            ++j;
+        }
     }
+
+    // push the remaining elements in v1 to v3
+    while (i < n)
+    {
+        v3.push_back(v1[i]);
+        ++i;
+    }
+
+    // push the remaining elements in v2 to v3
+    while (j < m)
+    {
+        v3.push_back(v2[j]);
+        ++j;
+    }
+
+    return v3;
+};
+
+// merge sort
+std::vector<int> MergeSort(const std::vector<int> &A)
+{
+    // initialize
+    auto n = A.size();
+    // base case
+    if (n == 1)
+        return A;
+
+    // get the mid size
+    auto m = n / 2;
+    // get the sliced vectors
+    auto left_slice = slice(A, 0, m);
+    auto right_slice = slice(A, m, n);
+    // for the left half
+    auto B = MergeSort(left_slice);
+    // for the right half
+    auto C = MergeSort(right_slice);
+    // merge two vectors
+    auto D = Merge(B, C);
+    // return
+    return D;
 }
 
 int main()
 {
     // initialize
-    int MaxN = 50;
+    int MaxN = 20;
     int MaxRand = 100;
     int MaxItr = 50;
     int Itr = 0;
@@ -81,15 +146,15 @@ int main()
             std::cout << "The vector BEFORE sorting: " << std::endl;
             printVec(test_vecA[i]);
             // call the sorting function
-            SelectionSort(test_vecA[i]);
+            auto merged_vec = MergeSort(test_vecA[i]);
             // print vector
             std::cout << "The vector AFTER sorting: " << std::endl;
-            printVec(test_vecA[i]);
+            printVec(merged_vec);
             std::cout << "-------------------------------------------------------------" << std::endl;
             // check
-            for (int j = 1; j < test_vecA[i].size(); ++j)
+            for (int i = 1; i < merged_vec.size(); ++i)
             {
-                if (test_vecA[i][j] < test_vecA[i][j - 1])
+                if (merged_vec[i] < merged_vec[i-1])
                     throw std::invalid_argument("the sorting algorithm is not right!");
             }
         }
